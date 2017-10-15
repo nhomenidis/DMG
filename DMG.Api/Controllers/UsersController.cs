19d9f75a331b3a57039f41ef156e3f8e3e10612c
Nikos Chomenidis <nhomenidis@me.com>
2017-10-16 00:18:45 +0300

    Create DMG solution and API project

---
+++ DMG.Api/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DMG.Business.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DMG.Api.Controllers
+{
+    [Route("api/users")]
+    public class UsersController : Controller
+    {
+        [HttpGet("{vat}")]
+        public IActionResult Get(string vat)
+        {
+            var userservice = new UserService();
+            var userdto = userservice.GetUser(vat);
+            return Ok(userdto);
+        }
+    }
+}
