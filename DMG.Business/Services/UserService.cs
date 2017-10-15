8094ecfac33f6b498c88aa0c65f78a6de3f0a47a
Nikos Chomenidis <nhomenidis@me.com>
2017-10-16 00:21:56 +0300

    Create User Service

---
+++ DMG.Business/Services/UserService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DMG.Business.Database;
+using DMG.Business.Dtos;
+using DMG.Business.Mappers;
+using DMG.Model;
+
+namespace DMG.Business.Services
+{
+    public interface IUserService
+    {
+        UserDto GetUser(string vat);
+
+    }
+
+    public class UserService : IUserService
+    {
+       public UserDto GetUser(string vat)
+       {
+           var userRepository = new UserRepository();
+           var user = userRepository.GetById(vat);
+
+           var mapper = new UserMapper();
+           var userdto = mapper.Map(user);
+
+           return userdto;
+       }
+    }
+
+}
