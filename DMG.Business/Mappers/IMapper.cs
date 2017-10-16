203f117453e354f949111d5c013d90ce120016b0
Nikos Chomenidis <nhomenidis@me.com>
2017-10-16 13:17:36 +0300

    Update Imapper with Generic Input

--- DMG.Business/Mappers/IMapper.cs
+++ DMG.Business/Mappers/IMapper.cs
@@ -6,6 +6,6 @@ namespace DMG.Business.Mappers
     public interface IMapper<TInput,TOutput>
     {
         TOutput Map(TInput user);
-        IEnumerable<TOutput> Map(IEnumerable<User> users);
+        IEnumerable<TOutput> Map(IEnumerable<TInput> users);
     }
 }
