4f892a1afdd09c80190744a7b507bbaa940fd49e
Nikos Chomenidis <nhomenidis@me.com>
2017-10-21 22:17:00 +0300

    Adding Swagger

--- DMG.Api/Controllers/UserController.cs
+++ DMG.Api/Controllers/UserController.cs
@@ -1,57 +1,57 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using DMG.DatabaseContext;
+﻿//using System;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+//using DMG.DatabaseContext;
 
-namespace DMG.Api.Controllers
-{
-    [Route("api/users")]
-    public class UserController : Controller
-    {
-        private UnitOfWork db;
+//namespace DMG.Api.Controllers
+//{
+//    [Route("api/users")]
+//    public class UserController : Controller
+//    {
+//        private UnitOfWork db;
 
-        public UserController(UnitOfWork u)
-        {
-            db = u;
-        }
+//        public UserController(UnitOfWork u)
+//        {
+//            db = u;
+//        }
 
-        [HttpGet("{vat}")]
-        public async Task<IActionResult> GetbyId(string id)
-        {
-            try
-            {
-               var user = await db.UserRepository.GetById(id);
+//        [HttpGet("{vat}")]
+//        public async Task<IActionResult> GetbyId(string id)
+//        {
+//            try
+//            {
+//               var user = await db.UserRepository.GetById(id);
 
 
-                return Ok(user);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest();
+//                return Ok(user);
+//            }
+//            catch (Exception ex)
+//            {
+//                return BadRequest();
 
-            }
+//            }
 
-        }
+//        }
 
     
 
-        [HttpPatch("{oldpass}/{newpass}")]
-        public IActionResult UpdateAccount(string oldpass, string newpass)
-        {
-            try
-            {
+//        [HttpPatch("{oldpass}/{newpass}")]
+//        public IActionResult UpdateAccount(string oldpass, string newpass)
+//        {
+//            try
+//            {
 
-             return Ok(true);
+//             return Ok(true);
 
-            }
-            catch (Exception ex)
-            {
-                return BadRequest();
+//            }
+//            catch (Exception ex)
+//            {
+//                return BadRequest();
 
-            }
-         }
+//            }
+//         }
     
-    }
-}
+//    }
+//}
 
     
