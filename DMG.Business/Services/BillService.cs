72d1f52b9a571124258e33725f2b17ad524687e8
Nikos Chomenidis <nhomenidis@me.com>
2017-10-22 17:41:34 +0300

    Create Bill Dto - Bill Mapper - BillService and update bill model

---
+++ DMG.Business/Services/BillService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DMG.Business.Database;
+using DMG.Business.Dtos;
+using DMG.Business.Mappers;
+using DMG.Models;
+
+namespace DMG.Business.Services
+{
+    public interface IBillService
+        {
+            BillDto GetBill(string billId);
+            IEnumerable<BillDto> GetBillsbyUser(User user);
+        }
+
+    public class BillService : IBillService
+    {
+        public BillDto GetBill(string Id)
+        {
+            var billrepository = new BillRepository();
+            var bill = billrepository.GetById(Id);
+
+            var mapper = new BillMapper();
+            var billdto = mapper.Map(bill);
+
+            return billdto;
+
+        }
+
+        public IEnumerable<BillDto> GetBillsbyUser(User user)
+        {
+            var billrepository = new BillRepository();
+            var bills = billrepository.GetAll(user);
+
+            var mapper = new BillMapper();
+            var billsdto = mapper.Map(bills);
+
+            return billsdto;
+        }
+
+
+
+
+
+    }
+}
--- DMG.Models/Bill.cs
+++ DMG.Models/Bill.cs
@@ -6,9 +6,9 @@ namespace DMG.Models
 {
     public class Bill : EntityBase
     {
-
+        public string Id { get; set; }
         public string Description { get; set; }
-        public float Amount { get; set; }
+        public double Amount { get; set; }
         public DateTime DueDate { set; get; }
         public string Status { set; get; }
 
