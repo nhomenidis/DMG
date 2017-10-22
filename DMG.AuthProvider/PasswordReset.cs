81ece469011a15cd6b92ea6c5a21563f9153fe6e
Nikos Chomenidis <nhomenidis@me.com>
2017-10-22 17:42:26 +0300

    Remove Token property from Password Reset

--- DMG.AuthProvider/PasswordReset.cs
+++ DMG.AuthProvider/PasswordReset.cs
@@ -9,6 +9,6 @@ namespace DMG.AuthProvider
         public string Username { get; set; }
         public string OldPassword { get; set; }
         public string NewPassword { get; set; }
-        public string Token { get; set; }
+        
     }
 }
