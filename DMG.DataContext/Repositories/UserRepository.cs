21ebafb1d7c6ec23b6920f3a8f1d8a4ab65693b7
Nikos Chomenidis <nhomenidis@me.com>
2017-10-23 14:09:07 +0300

    Fix variables for a more self explanatory code

--- DMG.DataContext/Repositories/UserRepository.cs
+++ DMG.DataContext/Repositories/UserRepository.cs
@@ -4,58 +4,58 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace DMG.DatabaseContext
+namespace DMG.DatabaseContext.Repositories
 {
     public class UserRepository : IRepository<User>
     {
-        private DbSet<User> dbSet;
+       private DbSet<User> Users;
 
         public UserRepository(DataContext context)
         {
-            dbSet = context.Set<User>();
+            Users = context.Set<User>();
         }
 
         public async Task<User> GetByUsername(string username)
         {
-            var entity = await dbSet.FirstOrDefaultAsync(x => x.Vat == username);
+            var user = await Users.FirstOrDefaultAsync(x => x.FirstName+" "+ x.LastName == username);
 
-            return entity;
+            return user;
         }
 
         public async Task<bool> Delete(string id)
         {
-            var entity = await dbSet.FindAsync(id);
+            var user = await Users.FindAsync(id);
 
-            dbSet.Remove(entity);
+            Users.Remove(user);
 
             return true;
         }
 
         public async Task<User> GetById(string id)
         {
-            var entity = await dbSet.FindAsync(id);
+            var user = await Users.FindAsync(id);
 
-            return entity;
+            return user;
         }
 
         public async Task<User> Insert(User entity)
         {
-            var user = await dbSet.AddAsync(entity);
+            var user = await Users.AddAsync(entity);
 
             return user.Entity;
         }
 
-        public bool InsertMany(List<User> entities)
+        public bool  InsertMany(List<User> users) // can't we make this asynchronously?
         {
-            dbSet.AddRange(entities);
+           Users.AddRange(users);
 
-            return true;
+           return true;
         }
         
 
         public User Update(User entity)
         {
-            var user = dbSet.Update(entity);
+            var user = Users.Update(entity);
 
             return user.Entity;
         }
