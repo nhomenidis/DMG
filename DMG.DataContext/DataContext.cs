727973c0499b09e43c0477f8f264022b1113bcc9
Nikos Chomenidis <nhomenidis@me.com>
2017-10-23 14:09:37 +0300

    Create DB Sets

--- DMG.DataContext/DataContext.cs
+++ DMG.DataContext/DataContext.cs
@@ -1,14 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Security.Cryptography.X509Certificates;
+using DMG.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DMG.DatabaseContext
 {
     public class DataContext : DbContext
     {
-
-        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
         }
+
+        public DbSet<Bill> Bills { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Settlement> Settlements { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<AddressInfo> AddressInfos { get; set; }
+        
+        
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //}
     }
 }
