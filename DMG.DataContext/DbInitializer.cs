2fbd919e0518fde89f55ca5cd36a2597c6d2346b
Nikos Chomenidis <nhomenidis@me.com>
2017-10-23 14:07:24 +0300

    Initialize Database without data

---
+++ DMG.DataContext/DbInitializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DMG.DatabaseContext
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(DataContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
