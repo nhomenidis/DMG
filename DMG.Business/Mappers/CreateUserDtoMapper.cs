e5052bb5547e72e44d5cc1e67f3bbbe94a1f8470
Nikos Chomenidis <nhomenidis@me.com>
2017-10-25 15:40:29 +0300

    Creates CreateUserDto and ti's mapper to User

---
+++ DMG.Business/Mappers/CreateUserDtoMapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DMG.AuthProvider;
+using DMG.Business.Dtos;
+using DMG.Models;
+
+namespace DMG.Business.Mappers
+{
+    public class CreateUserDtoMapper : IMapper<CreateUserDto, User>
+    {
+        public User Map(CreateUserDto userDto)
+        {
+            return new User
+            {
+                FirstName = userDto.FirstName,
+                LastName = userDto.LastName,
+                Email = userDto.Email,
+                Vat = userDto.Vat,
+                Password = PasswordHasher.HashPassword(userDto.Password)
+            };
+        }
+
+        public IEnumerable<User> Map(IEnumerable<CreateUserDto> userDtos)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
