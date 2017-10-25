5440c334c87e9959a824384a18ed3f1d525f5de2
Nikos Chomenidis <nhomenidis@me.com>
2017-10-25 15:32:14 +0300

    Updates users and bill controllers

--- DMG.Api/Controllers/BillController.cs
+++ DMG.Api/Controllers/BillController.cs
@@ -1,35 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
-using DMG.Business.Dtos;
-using DMG.Business.Services;
-using DMG.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net;
+//using System.Threading.Tasks;
+//using DMG.Business.Dtos;
+//using DMG.Business.Services;
+//using DMG.Models;
+//using Microsoft.AspNetCore.Mvc;
 
-namespace DMG.Api.Controllers
-{
-    [Route("api/bills")]
+//namespace DMG.Api.Controllers
+//{
+//    [Route("api/bills")]
 
-    public class BillController : Controller
-    {
+//    public class BillController : Controller
+//    {
 
-        [HttpGet("{id}")]
-        public IActionResult GetbyId(string id)
-        {
-            var billservice = new BillService();
-            var billdto = billservice.GetBill(id);
-            return Ok(billdto);
+//        [HttpGet("{id}")]
+//        public IActionResult GetbyId(string id)
+//        {
+//            var billservice = new BillService();
+//            var billdto = billservice.GetBill(id);
+//            return Ok(billdto);
 
-        }
+//        }
 
-        [HttpGet]
-        public IActionResult GetByUser(User user)
-        {
-            var billservice = new BillService();
-            var billdto = billservice.GetBillsbyUser(user);
-            return Ok(billdto);
-        }
-    }
-}
+//        [HttpGet]
+//        public IActionResult GetByUser(User user)
+//        {
+//            var billservice = new BillService();
+//            var billdto = billservice.GetBillsbyUser(user);
+//            return Ok(billdto);
+//        }
+//    }
+//}
--- DMG.Api/Controllers/UsersController.cs
+++ DMG.Api/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DMG.AuthProvider;
+using DMG.Business.Dtos;
 using DMG.Business.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,30 +12,59 @@ namespace DMG.Api.Controllers
     [Route("api/users")]
     public class UsersController : Controller
     {
-        [HttpGet("{vat}")]
-        public IActionResult Get(string vat)
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
         {
-            var userservice = new UserService();
-            var userdto = userservice.GetUser(vat);
+            _userService = userService;
+        }
+
+        [HttpGet("{vat}")] // gets user info by VAT
+        public async Task<IActionResult> Get(string vat)
+        {
+            var userdto = await _userService.GetUser(vat);
+            if (userdto == null)
+            {
+                return NotFound("User not found");
+            }
             return Ok(userdto);
         }
 
-        [HttpGet]
-        public IActionResult GetAll()
+        [HttpGet] // get all users
+        public async Task<IActionResult> GetAll()
         {
-            var userservice = new UserService();
-            var usersdto = userservice.GetAll();
+            var usersdto = await _userService.GetAll();
             return Ok(usersdto);
         }
 
-        [HttpPatch("{changePass}")]
-        public IActionResult UpdateAccount(PasswordReset newpass)
+        [HttpPatch("{vat}/password")] // change password
+        public async Task<IActionResult> UpdateAccount(string vat, [FromBody]PasswordReset resetRequest)
         {
-            var userservice = new UserService();
-
-            return Ok();
-
+            var oldPassword = resetRequest.OldPassword;
+            var newPassword = resetRequest.NewPassword;
+            var result = await _userService.ChangePassword(vat, oldPassword, newPassword);
+            if (result)
+            {
+                return Ok();
+            }
+            return Unauthorized();
         }
+
+        [HttpPost] // create a new user
+        public async Task<IActionResult> CreateUser([FromBody] CreateUserDto newUser)
+        {
+            var result = await _userService.CreateUser(newUser);
+
+            return Ok(result);
+        }
+
+        //[HttpPatch("{newEmail}")] //change email
+        //public IActionResult ChangeMail(string newEmail)
+        //{
+        //    var userservice = new UserService();
+
+
+        //}
 
     }
 }
