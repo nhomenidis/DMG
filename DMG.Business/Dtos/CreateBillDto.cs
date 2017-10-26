52238de798d22d00e43cb206eed3d71b98a46ab6
Nikos Chomenidis <nhomenidis@me.com>
2017-10-26 09:32:06 +0300

    creates BillDto and ti's mapper to Bill

---
+++ DMG.Business/Dtos/CreateBillDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DMG.Business.Dtos
+{
+    public class CreateBillDto
+    {
+        public string Description { get; set; }
+        public double Amount { get; set; }
+        public DateTime DueDate { set; get; }
+        public bool IsPayed { set; get; }
+
+        public Guid UserId { get; set; }
+    }
+}
---
+++ DMG.Business/Mappers/CreateBillDtoMapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DMG.Business.Dtos;
+using DMG.Models;
+
+namespace DMG.Business.Mappers
+{
+    public class CreateBillDtoMapper : IMapper<CreateBillDto, Bill>
+    {
+        public Bill Map(CreateBillDto billDto)
+        {
+            return new Bill
+            {
+                Description = billDto.Description,
+                Amount = billDto.Amount,
+                DueDate = billDto.DueDate,
+                IsPayed = billDto.IsPayed,
+                UserId = billDto.UserId
+            };
+        }
+
+        public IEnumerable<Bill> Map(IEnumerable<CreateBillDto> billDtos)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
