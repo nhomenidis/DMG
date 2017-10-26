31a53e72df8603c8673a5bc8693408a388f7dc0e
Nikos Chomenidis <nhomenidis@me.com>
2017-10-26 09:29:09 +0300

    Updates bill controller with dependency injection

--- DMG.Api/Controllers/BillController.cs
+++ DMG.Api/Controllers/BillController.cs
@@ -1,35 +1,49 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Net;
-//using System.Threading.Tasks;
-//using DMG.Business.Dtos;
-//using DMG.Business.Services;
-//using DMG.Models;
-//using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using DMG.Business.Dtos;
+using DMG.Business.Mappers;
+using DMG.Business.Services;
+using DMG.Models;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace DMG.Api.Controllers
-//{
-//    [Route("api/bills")]
+namespace DMG.Api.Controllers
+{
+    [Route("api/bills")]
+    public class BillController : Controller
+    {
+        private readonly IBillService _billService;
+        private readonly IUserService _userService;
 
-//    public class BillController : Controller
-//    {
+        public BillController(IBillService billService, IUserService userService) 
+        {
+            _billService = billService;
+            _userService = userService;
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetbyId(Guid id)
+        {
+            var billdto = await _billService.GetBill(id);
 
-//        [HttpGet("{id}")]
-//        public IActionResult GetbyId(string id)
-//        {
-//            var billservice = new BillService();
-//            var billdto = billservice.GetBill(id);
-//            return Ok(billdto);
+            return Ok(billdto);
 
-//        }
+        }
 
-//        [HttpGet]
-//        public IActionResult GetByUser(User user)
-//        {
-//            var billservice = new BillService();
-//            var billdto = billservice.GetBillsbyUser(user);
-//            return Ok(billdto);
-//        }
-//    }
-//}
+        [HttpGet("User")]
+        public async Task<IActionResult> GetByUser(User user)
+        {
+            var bills = await _billService.GetBillsbyUser(user);
+            return Ok(bills);
+        }
+
+        [HttpPost] // create a new bill
+        public async Task<IActionResult> CreateBill([FromBody] CreateBillDto newBill)
+        {
+            var result = await _billService.CreateBill(newBill);
+
+            return Ok(result);
+        }
+    }
+}
