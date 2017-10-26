c7fa8d57ef881cdc92f20c3adad3f8c5fb899b1a
Nikos Chomenidis <nhomenidis@me.com>
2017-10-26 09:29:36 +0300

    adds fileds in BillDto

--- DMG.Business/Dtos/BillDto.cs
+++ DMG.Business/Dtos/BillDto.cs
@@ -10,6 +10,9 @@ namespace DMG.Business.Dtos
         public DateTime DueDateTime { get; set; }
         public Boolean IsPayed { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
 
     }
 
