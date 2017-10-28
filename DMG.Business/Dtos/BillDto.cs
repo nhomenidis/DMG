2dbeeccb87bb26121ff09b860e9ea39560b248bd
Nikos Chomenidis <nhomenidis@me.com>
2017-10-28 23:54:15 +0300

    Enriches DTOs

--- DMG.Business/Dtos/BillDto.cs
+++ DMG.Business/Dtos/BillDto.cs
@@ -7,12 +7,13 @@ namespace DMG.Business.Dtos
     {
         public string BillId { get; set; }
         public double Amount { get; set; }
-        public DateTime DueDateTime { get; set; }
-        public Boolean IsPayed { get; set; }
-        public string Name { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime DatePayed { get; set; }
+        public bool IsPayed { get; set; }
         public string Description { get; set; }
         public string UserFirstName { get; set; }
         public string UserLastName { get; set; }
+        public string Vat { get; set; }
 
         public Guid UserId { get; set; }
 
--- DMG.Business/Dtos/UserDto.cs
+++ DMG.Business/Dtos/UserDto.cs
@@ -2,8 +2,11 @@
 {
     public class UserDto
     {
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Vat { get; set; }
+        public string Address { get; set; }
+        public string County { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
 
