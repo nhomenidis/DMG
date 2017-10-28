787fdfd2768bbde9ef60a8ee0c5d5bc6e36b59cb
Nikos Chomenidis <nhomenidis@me.com>
2017-10-28 23:54:55 +0300

    Creates new Dto objects

---
+++ DMG.Business/Dtos/CreateSettlementDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using DMG.Models;
+
+namespace DMG.Business.Dtos
+{
+    public class CreateSettlementDto
+    {
+        public SettlementType Type { get; set; }
+        public int Installements { get; set; }
+        public string Vat { get; set; }
+        public IEnumerable<BillDto> BillsInSettlement { get; set; }
+        
+    }
+}
---
+++ DMG.Business/Dtos/SettlementsDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DMG.Business.Dtos
+{
+    public class SettlementsDto
+    {
+        public Guid Id { get; set; }
+        public DateTime DateRequeste { get; set; }
+        public IEnumerable<BillDto> Bills { get; set; }
+        public double TotalAmount { get; set; }
+        public double DownPayment { get; set; }
+        public int Installements { get; set; }
+        public double Interest { get; set; }
+        public string Vat { get; set; }
+
+    }
+}
