9694e64a366ce37fd3c59b7145efa500beb26d21
Nikos Chomenidis <nhomenidis@me.com>
2017-11-02 14:05:22 +0200

    Introduces Parse Model, for the File Helpers parser to work with

---
+++ DMG.Models/ParseModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FileHelpers;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace DMG.Models
+{
+    [IgnoreFirst(1)]
+    [DelimitedRecord(";")]
+    public class ParseModel
+    {
+        public string VAT;
+        public string FIRST_NAME;
+        public string LAST_NAME;
+        public string EMAIL;
+        public string PHONE;
+        public string ADRESS;
+        public string COUNTY;
+
+        [FieldConverter(ConverterKind.Guid, "D")]
+        public Guid BILL_ID;
+
+        public string DESCRIPTION;
+
+        [FieldConverter(ConverterKind.Double, "," )]
+        public double AMOUNT;
+
+       [FieldConverter(ConverterKind.Date, "yyyyMMdd")]
+        public DateTime DATE_DUE;
+
+    }
+}
+
+
+//        public string VAT 
+//        public string FIRST_NAME { get; set; }
+//        public string LAST_NAME { get; set; }
+//        public string EMAIL { get; set; }
+//        public string PHONE { get; set; }
+//        public string ADRESS { get; set; }
+//        public string COUNTY { get; set; }
+//        public string BILL_ID { get; set; }
+//        public string DESCRIPTION { get; set; }
+//        public string AMOUNT { get; set; }
+//        public string DATE_DUE { set; get; }
