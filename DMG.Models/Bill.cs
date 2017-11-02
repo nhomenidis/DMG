3b24be85dd815a8a353f5bfd0612dbeea46db178
Nikos Chomenidis <nhomenidis@me.com>
2017-11-02 14:03:30 +0200

    minor changes to Domain models

--- DMG.Models/Bill.cs
+++ DMG.Models/Bill.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using FileHelpers;
+
 
 namespace DMG.Models
 {
-    public class Bill : EntityBase
+    public class Bill : EntityBase //(no need for an extra Id because the bill comes ready with it's own Guid Bill Id
     {
+        public Guid BillId { get; set; }
         public string Description { get; set; }
         public double Amount { get; set; }
-        public DateTime DueDate { set; get; }
+        public DateTime DateDue { set; get; }
+        public DateTime DatePayed { get; set; }
         public bool IsPayed { set; get; }
 
         public Guid UserId { get; set; } // foreign key
         public User User { get; set; } //navigation property
+        public string UserVat { get; set; }
     }
 }
--- DMG.Models/Settlement.cs
+++ DMG.Models/Settlement.cs
@@ -9,14 +9,14 @@ namespace DMG.Models
         public double DownPayment { get; set; }
         public int Installements { get; set; }
         public double Interest { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime DateRequested { get; set; }
         public SettlementType Type { get; set; } //we enumerate the 5 concrete types of settlements
 
         //navigation properties
         public ICollection<Bill> Bills { get; set; }
 
         public Guid UserId { get; set; } // foreign key
-        public User User { get; set; }
+        public string UserVat { get; set; }
     }
 
     public enum SettlementType : short
