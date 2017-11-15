bad24c1f9bd01e6126a6a597122d7b0a08fa304c
Nikos Chomenidis <nhomenidis@me.com>
2017-11-15 12:22:33 +0200

    Adds Settlements DTO and an ErrorDto

---
+++ DMG.Business/Dtos/CalculatedSettlementDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DMG.Business.Dtos
+{
+    public class CalculatedSettlementDto
+    {
+        public double DownPayment { get; set; }
+        public int Installements { get; set; }
+        public double Interest { get; set; }
+        public double InstallementAmount { get; set; }  
+    }
+}
---
+++ DMG.Business/Dtos/SettlementTypeDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DMG.Business.Dtos
+{
+    public class SettlementTypeDto
+    {
+        public short TypeNumber { get; set; }
+        public string TypeName { get; set; }
+        public string Formula { get; set; }
+
+    }
+}
